65a156b411ecf733bca158f2505a3088c5c9f3b5
WeiZheng78 <weizheng78@gmail.com>
2021-08-20 11:04:28 +0800

    interact with mysql

--- code/controller/sample.php
+++ code/controller/sample.php
@@ -2,8 +2,21 @@
 
     function page() {
         $_rc = []; 
-        $someVar = [ "foo" => "bar" ];
-        array_push($_rc, $someVar);
+        $mysqli = mysqli_init();
+        $mysqli->real_connect("database-1.clqonggx5zd5.ap-southeast-1.rds.amazonaws.com","admin",{password},'school',3306);
+        if ($mysqli->connect_errno) {
+            $connVar = [ $mysqli->connect_errno => $mysqli->connect_error ];
+            array_push($_rc, $connVar);
+        } else {
+            $studentRecords = array();
+            $queryresult = mysqli_query($mysqli,"SELECT * FROM student;");
+            while($cRow = mysqli_fetch_array($queryresult))
+            {
+                array_push($studentRecords,$cRow);
+            }
+            array_push($_rc, $studentRecords);
+        }
+        $mysqli->close();
         return $_rc;
     }
     
@@ -15,5 +28,18 @@
     function result() {
         $_rc = [];
         array_push($_rc, $_POST);
+        $mysqli = mysqli_init();
+        $mysqli->real_connect("database-1.clqonggx5zd5.ap-southeast-1.rds.amazonaws.com","admin",{password},'school',3306);
+        if ($mysqli->connect_errno) {
+            $connVar = [ $mysqli->connect_errno => $mysqli->connect_error ];
+            array_push($_rc, $connVar);
+        } else {
+            $queryresult = mysqli_query($mysqli,"insert into student (name) values ('".$_rc[0]['name']."')");
+            if(!$queryresult)
+            {
+                array_push($_rc, $queryresult);
+            }
+        }
+        $mysqli->close();
         return $_rc;
     }
